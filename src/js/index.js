54a13d19b18bcde21f0b0d335a6c0fb24690b611
SebastianCieslik <ssssss-94@o2.pl>
2022-01-03 20:41:18 +0100

    Added Moment.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,24 @@
-import '../scss/style.scss';
+import "../scss/style.scss";
 
+const menus = document.querySelector(".menu--js");
+const nav = document.querySelector(".navigation--js");
 
-const menus = document.querySelector('.menu--js');
-const nav = document.querySelector('.navigation--js');
+menus.addEventListener("click", () => {
+  nav.classList.toggle("navigation__open");
+  console.log("Klik");
+});
+//Clock
+const clock = document.getElementById("clock");
 
-menus.addEventListener('click', () =>{ 
-    nav.classList.toggle('navigation__open');
-    console.log("Klik");
-})
+function updateTime() {
+  moment.locale("pl");
+  const now = moment();
 
-
+  const humanReadable = now.format("MMMM Do YYYY, h:mm:ss a");
+  clock.textContent = humanReadable;
+}
+setInterval(updateTime, 1000);
+updateTime();
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -17,4 +26,4 @@ menus.addEventListener('click', () =>{
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
